474400c3fc9913bce8e08f41f55e4ec211ce71ab
Vinay <mothkulavinaygoud@gmail.com>
2026-01-31 11:13:09 -0600

    Initial Commit

---
+++ WhatsAppStatusTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppStatusTableView
+//
+//  Created by Vinay Goud Mothkula on 1/31/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
